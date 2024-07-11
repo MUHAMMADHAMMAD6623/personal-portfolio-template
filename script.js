ebb8a538cf9581eb1d5ae5ae6a969594189991ae
MUHAMMADHAMMAD6623 <142398205+MUHAMMADHAMMAD6623@users.noreply.github.com>
2024-07-11 16:36:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const hamburgerIcon = document.querySelector('.menu');
+   const rightSection = document.querySelector('.right');
+ 
+   function setInitialHeight() {
+     if (window.innerWidth < 800) {
+       rightSection.style.height = '0px';
+     } else {
+       rightSection.style.height = 'auto';
+     }
+   }
+ 
+   setInitialHeight();
+   window.addEventListener('resize', setInitialHeight);
+ 
+   hamburgerIcon.addEventListener('click', () => {
+     hamburgerIcon.classList.toggle('menu-clicked');
+     if (rightSection.style.height === '0px') {
+       rightSection.style.height = '100vh';
+     } else {
+       rightSection.style.height = '0px';
+     }
+   });
+ });
+ 
