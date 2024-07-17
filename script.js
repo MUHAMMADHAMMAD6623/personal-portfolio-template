f6a8b6f5a929addfe0fd925c6ad8af4593e3ab2d
MUHAMMADHAMMAD6623 <142398205+MUHAMMADHAMMAD6623@users.noreply.github.com>
2024-07-17 15:03:21 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // variables
   const hamburgerIcon = document.querySelector(".menu");
   const rightSection = document.querySelector(".right");
-
+  const universe = document.querySelector('body')
+  // Function for the hamburger menu
   function setInitialHeight() {
     if (window.innerWidth < 900) {
       rightSection.style.height = "0px";
@@ -12,16 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setInitialHeight();
   window.addEventListener("resize", setInitialHeight);
-
+  // Function for the hamburger menu
   hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("menu-clicked");
+    const sectionhiding = setTimeout(()=>{
+      universe.classList.toggle('not-needed')
+    },600)
     if (rightSection.style.height === "0px") {
       rightSection.style.height = "100vh";
     } else {
       rightSection.style.height = "0px";
     }
   });
-
+  // JavaScript code for smooth scrolling on clicking the navbar links
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
@@ -31,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
-
+  // Swiper js object
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 2,
     spaceBetween: 30,
@@ -43,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
       clickable: true,
     },
   });
-
+  // Js media query to set 1 column instead of 2 in devices having width less than 700px
   const jsmediaquery = () => {
     if (mediaquery.matches) {
       var swiper = new Swiper(".mySwiper", {
